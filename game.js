6a6e0a14b35a10ddf35b7d73860f142839a7d6ed
Aman Gupta <95572040+heyamangupta@users.noreply.github.com>
2023-06-18 16:45:15 +0530

    error fix

--- game.js
+++ game.js
@@ -40,7 +40,7 @@ function checkAnswer(currentLevel) {
         playSound("wrong");
         $("body").addClass("game-over");
 
-        $("#level-title").text("Game Over, Press Any Key to Restart");
+        $(".level-title").text("Game Over, Press Any Key to Restart");
 
         setTimeout(function () {
             $("body").removeClass("game-over");
